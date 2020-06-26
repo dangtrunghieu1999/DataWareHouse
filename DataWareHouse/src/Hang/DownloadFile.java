5d11e14648031c968327ccab5a127630cd73b413
nguyenthithuyhang11 <55865873+nguyenthithuyhang11@users.noreply.github.com>
2020-06-26 23:30:00 +0700

    Sửa lỗi
    Lỗi bị sai khi không thể tìm hoặc load main class Hang.DownloadFile

--- DataWareHouse/src/Hang/DownloadFile.java
+++ DataWareHouse/src/Hang/DownloadFile.java
@@ -10,57 +10,31 @@ public class DownloadFile {
 
 		try {
 			JSch ssh = new JSch();
-			// JSch.setConfig("StrictHostKeyChecking", "no");
+			JSch.setConfig("StrictHostKeyChecking", "no");
 			session = ssh.getSession(userName, host, port);
-			//
-			java.util.Properties config = new java.util.Properties();
-			config.put("StrictHostKeyChecking", "no");
-			session.setConfig(config);
+
 			session.setPassword(password);
 			session.connect();
 			channel = session.openChannel("sftp");
 			channel.connect();
 			ChannelSftp sftp = (ChannelSftp) channel;
-			sftp.cd(pathDir);
-
-			sftp.get("/ECEP/song.nguyen/DW_2020/data/sinhvien_chieu_nhom6.xlsx", pathDir);
-			Boolean success = true;
-			if (success) {
-				// The file has been succesfully downloaded
-			}
-
-			channel.disconnect();
-			session.disconnect();
+			sftp.get(pathDir, "specify path to where you want the files to be output");
 		} catch (JSchException e) {
-			System.out.println(e.getMessage().toString());
+			System.out.println(userName);
+			System.out.println(password);
 			e.printStackTrace();
-		} catch (SftpException e) {
-			System.out.println(e.getMessage().toString());
-			e.printStackTrace();
-		}
 
-		// session.setPassword(password);
-		// session.connect();
-		// channel = session.openChannel("sftp");
-		// channel.connect();
-		// ChannelSftp sftp = (ChannelSftp) channel;
-		// sftp.get(pathDir, "specify path to where you want the files to be output");
-		// } catch (JSchException e) {
-		// System.out.println(userName);
-		// System.out.println(password);
-		// e.printStackTrace();
-		//
-		// } catch (SftpException e) {
-		// System.out.println(userName);
-		// e.printStackTrace();
-		// } finally {
-		// if (channel != null) {
-		// ((ChannelSftp) channel).disconnect();
-		// }
-		// if (session != null) {
-		// session.disconnect();
-		// }
-		// }
+		} catch (SftpException e) {
+			System.out.println(userName);
+			e.printStackTrace();
+		} finally {
+			if (channel != null) {
+				((ChannelSftp) channel).disconnect();
+			}
+			if (session != null) {
+				session.disconnect();
+			}
+		}
 
 	}
 
