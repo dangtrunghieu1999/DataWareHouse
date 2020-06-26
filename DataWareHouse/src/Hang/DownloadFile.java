78b65dbfde0c30733584da6d569ae109b8c651ad
nguyenthithuyhang11 <55865873+nguyenthithuyhang11@users.noreply.github.com>
2020-06-26 10:21:32 +0700

    Sửa lỗi
    Could not find or load main class Hang.DownloadFile

--- DataWareHouse/src/Hang/DownloadFile.java
+++ DataWareHouse/src/Hang/DownloadFile.java
@@ -1,79 +1,66 @@
 package Hang;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import com.jcraft.jsch.*;
 
 public class DownloadFile {
-	private Connection connection;
 
-	public void loadFromSourceFile() {
-		System.out.println("connect success");
-		try {
-			Statement st = connection.createStatement();
-			ResultSet rs = st.executeQuery("select * from logs join config on config.id = logs.id");
-			String file_name, status, src_type, delimited,source, des,user_des,pw_des,field;
-			int id;
-
-			while (rs.next()) {
-				id        	= rs.getInt("id");
-				file_name 	= rs.getString("file_name");
-				status 	  	= rs.getString("status");
-				src_type  	= rs.getString("src_type");
-				delimited 	= rs.getString("delimited");
-				source 	  	= rs.getString("source");
-				des 		= rs.getString("destination");
-				user_des	= rs.getString("user_des");
-				pw_des  	= rs.getString("pw_des");
-				field	    = rs.getString("field");
-				
-				if (src_type.equals("xlsx")) {
-//					loadFromXLSX(id, status, file_name, source, des, user_des, pw_des, delimited, field);
-				}
-			
-				
-			}
-			
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-	
 	public void downloadFtp(String userName, String password, String host, int port, String pathDir) {
 		Session session = null;
 		Channel channel = null;
 
 		try {
 			JSch ssh = new JSch();
-			JSch.setConfig("StrictHostKeyChecking", "no");
+			// JSch.setConfig("StrictHostKeyChecking", "no");
 			session = ssh.getSession(userName, host, port);
+			//
+			java.util.Properties config = new java.util.Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
 			session.setPassword(password);
 			session.connect();
 			channel = session.openChannel("sftp");
 			channel.connect();
 			ChannelSftp sftp = (ChannelSftp) channel;
-			sftp.get(pathDir, "specify path to where you want the files to be output");
-		} catch (JSchException e) {
-			System.out.println(userName);
-			System.out.println(password);
-			e.printStackTrace();
+			sftp.cd(pathDir);
 
-		} catch (SftpException e) {
-			System.out.println(userName);
+			sftp.get("/ECEP/song.nguyen/DW_2020/data/sinhvien_chieu_nhom6.xlsx", pathDir);
+			Boolean success = true;
+			if (success) {
+				// The file has been succesfully downloaded
+			}
+
+			channel.disconnect();
+			session.disconnect();
+		} catch (JSchException e) {
+			System.out.println(e.getMessage().toString());
 			e.printStackTrace();
-		} finally {
-			if (channel != null) {
-				((ChannelSftp) channel).disconnect();
-			}
-			if (session != null) {
-				session.disconnect();
-			}
+		} catch (SftpException e) {
+			System.out.println(e.getMessage().toString());
+			e.printStackTrace();
 		}
+
+		// session.setPassword(password);
+		// session.connect();
+		// channel = session.openChannel("sftp");
+		// channel.connect();
+		// ChannelSftp sftp = (ChannelSftp) channel;
+		// sftp.get(pathDir, "specify path to where you want the files to be output");
+		// } catch (JSchException e) {
+		// System.out.println(userName);
+		// System.out.println(password);
+		// e.printStackTrace();
+		//
+		// } catch (SftpException e) {
+		// System.out.println(userName);
+		// e.printStackTrace();
+		// } finally {
+		// if (channel != null) {
+		// ((ChannelSftp) channel).disconnect();
+		// }
+		// if (session != null) {
+		// session.disconnect();
+		// }
+		// }
 
 	}
 
@@ -83,7 +70,7 @@ public class DownloadFile {
 		int port = 2227;
 		String userName = "guest_access";
 		String password = "123456";
-		String pathDir = "F:\\HK6-2020\\DataWareHouse_ThaySong\\FileNopBai";
+		String pathDir = "F:\\HK6-2020\\DataWareHouse_ThaySong\\FileNopBai\\sinhvien_chieu_nhom6.xlsx";
 		download.downloadFtp(userName, password, hostname, port, pathDir);
 	}
 }
