cc402e2df666ea2efa7dedbb568fd62efaeaacb3
Dang Trung Hieu <hieudt230899@gmail.com>
2020-07-13 09:00:39 +0700

    * Update load xlsx vs csv or txt

--- DataWareHouse/src/etl/LoadData.java
+++ DataWareHouse/src/etl/LoadData.java
@@ -1,8 +1,10 @@
 package etl;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,6 +14,9 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.Cell;
@@ -20,12 +25,12 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+@SuppressWarnings("deprecation")
 public class LoadData {
 	
-	public static final String SQL_INSERT = "INSERT INTO ${table} VALUES (${values})";
-	public static final String TABLE_REGEX = "\\$\\{table\\}";
-	public static final String VALUES_REGEX = "\\$\\{values\\}";
+	private String NUMBER_REGEX = "1";
 	private Connection connection;
+	public int countRows = 0;
 
 	public LoadData(Connection connection) {
 		this.connection = connection;
@@ -37,7 +42,7 @@ public class LoadData {
 			Statement st = connection.createStatement();
 			ResultSet rs = st.executeQuery("select * from logs join config on config.id = logs.id");
 			String file_name, status, src_type, delimited,source, des,user_des,pw_des,field,table_name;
-			int id, ignore;
+			int id, count_field;
 
 			while (rs.next()) {
 				id        	= rs.getInt("id");
@@ -51,15 +56,15 @@ public class LoadData {
 				pw_des  	= rs.getString("pw_des");
 				field	    = rs.getString("field");
 				table_name	= rs.getString("table_name");
-				ignore 		= rs.getInt("ignore");
+				count_field = rs.getInt("count_field");
 				
 				if (src_type.equals("xlsx")) {
 					loadFromXLSX(id, status, file_name, source, des,
 							user_des, pw_des, delimited, field, table_name);
 				}
 				if (src_type.equals("csv") || src_type.equals("txt")) {
-					loadFromCSVOrTXTInfile(id, status, file_name, source, des, 
-							user_des, pw_des, delimited, field,table_name,ignore);
+					loadFromCSVOrTXT(id, status, file_name, source, des, 
+							user_des, pw_des, delimited, field,table_name,count_field);
 				}
 			}
 			
@@ -70,6 +75,7 @@ public class LoadData {
 
 	}
 	
+
 	public void changeStatusFile(String file_name, int numberRows) {
 		Date endDate = new Date();
 		Connection connection = null;
@@ -91,20 +97,17 @@ public class LoadData {
 			System.out.println("Change status error");
 		}
 	}
-	
-	@SuppressWarnings("deprecation")
+		
 	public void loadFromXLSX(int id, String status ,String file_name,
 							String source, String des, String user_des,
 							String pw_des, String delimited, String field, String table_name) {
 		String filePath = Support.filePath(source, file_name);
-		
 		Connection connection = null;
 		int batchSize = 20;
 		int countRows = 0;
+		int checkRows = 0;
 		
 		try {
-			
-			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(des, user_des, pw_des);
 			connection.setAutoCommit(false);
 			long start = System.currentTimeMillis();
@@ -115,8 +118,8 @@ public class LoadData {
 			Sheet firstSheet = workbook.getSheetAt(0);
 			Iterator<Row> rowIterator = firstSheet.iterator();
 
-			String query = Support.convertQuery(field);
-			System.out.println(query);
+			String query = Support.convertQuery(field, table_name);
+			
 			PreparedStatement statement = connection.prepareStatement(query);
 			
 			int count = 0;
@@ -126,12 +129,12 @@ public class LoadData {
 			while (rowIterator.hasNext()) {
 				Row nextRow = rowIterator.next();
 				Iterator<Cell> cellIterator = nextRow.cellIterator();
-
+				checkRows++;
 				while (cellIterator.hasNext()) {
+					
 					Cell nextCell = cellIterator.next();
-
 					int columnIndex = nextCell.getColumnIndex();
-
+					
 					switch (columnIndex) {
 					case 0:
 						int stt = (int) nextCell.getNumericCellValue();
@@ -159,91 +162,113 @@ public class LoadData {
 						default:
 							break;
 						}
-
 						counterSecond++;
 						break;
 					}
 				}
 				counterSecond = 2;
-
 				statement.addBatch();
-
+				
 				if (count % batchSize == 0) {
 					statement.executeBatch();
+					countRows++;
 				}
 			}
-
 			workbook.close();
-
-			// execute the remaining queries
-			statement.executeBatch();
-			
-			connection.commit();
-
-			long end = System.currentTimeMillis();
-			System.out.printf("Import done in %d ms\n", (end - start));
-			changeStatusFile(file_name, countRows);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}catch (Exception e) {
-			System.out.println(" loadToStaging that bai ");
-			MoveFileStatus.moveFileToError(filePath);
-		}
-		
-	}
-	
-	public void loadFromCSVOrTXTInfile(int id, String status ,String file_name,
-								String source, String des, String user_des,
-								String pw_des, String delimited,
-								String field,String table_name,int ignore) {
-		String filePath = Support.filePath(source, file_name);
-		String loadQuery = "LOAD DATA INFILE '" + filePath + "' INTO TABLE data FIELDS TERMINATED BY '\\"
-				+ delimited + "' LINES TERMINATED BY '\n' IGNORE " + ignore + " LINES";
-		System.out.println(loadQuery);
-		PreparedStatement statement;
-		Connection connection = null;
-		boolean check = false;
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(des, user_des, pw_des);
-			connection.setAutoCommit(false);
-			long start = System.currentTimeMillis();
-			
-			statement = connection.prepareStatement(loadQuery);
-			statement.executeUpdate();
-			statement.close();
 			connection.commit();
 			
-			long end = System.currentTimeMillis();
-			System.out.printf("Import done in %d ms\n", (end - start));
-//			changeStatusFile(file_name);
-			check = true;
-			
-			if (check) {
-//				changeStatusFile(file_name);
+			if (countRows == checkRows) {
+				long end = System.currentTimeMillis();
+				System.out.printf("Import done in %d ms\n", (end - start));
+				changeStatusFile(file_name, countRows);
 				MoveFileStatus.moveFileToSuccess(filePath);
 			} else {
-				// moveFileToError(path);
+				System.out.println(" loadToStaging that bai ");
+				MoveFileStatus.moveFileToError(filePath);
 			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
 		} catch (Exception e) {
 			System.out.println(" loadToStaging that bai ");
 			MoveFileStatus.moveFileToError(filePath);
 		}
+		
 	}
-
 	
+	private String readLines(String value, String delimited) {
+		String values = "";
+		StringTokenizer stoken = new StringTokenizer(value, delimited);
+		int countToken = stoken.countTokens();
+		String lines = "(";
+		String token = "";
+		for (int j = 0; j < countToken; j++) {
+			token = stoken.nextToken();
+			lines += (j == countToken - 1) ? '"' + token.trim() + '"' + ")," : '"' + token.trim() + '"' + ",";
+			values += lines;
+			lines = "";
+		}
+		return values;
+	}
+	
+	public String readValuesTXT(File s_file, int id_log, int count_field, String delimited) {
+		if (!s_file.exists()) {
+			return null;
+		}
+		String values = "";
+		
+		try {
+			BufferedReader bReader = new BufferedReader(new InputStreamReader(new FileInputStream(s_file)));
+			
+			String line = bReader.readLine();
+		
+			if (new StringTokenizer(line, delimited).countTokens() != count_field) {
+				bReader.close();
+				return null;
+			}
+			if (Pattern.matches(NUMBER_REGEX , line.split(delimited)[0])) {
+				values += readLines(line, delimited);
+			}
+			
+			while ((line = bReader.readLine()) != null) {
+
+				values += readLines(line + " ", delimited);
+				this.countRows++;
+			}
+			bReader.close();
+			return values.substring(0, values.length() - 1);
+
+		} catch (NoSuchElementException | IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public void loadFromCSVOrTXT(int id, String status, String file_name, String source, String des, String user_des,
+			String pw_des, String delimited, String field, String table_name, int count_field) {
+		
+		String filePath = Support.filePath(source, file_name);
+		File file = new File(filePath);
+		
+		String values = readValuesTXT(file, id, count_field,delimited);
+
+		Connection connection = null;
+		
+		String query = "INSERT INTO " + table_name + " VALUES" + " " + values;
+		System.out.println(this.countRows);
+		try {
+			long start = System.currentTimeMillis();
+			connection = DriverManager.getConnection(des, user_des, pw_des);
+			connection.setAutoCommit(false);
+
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.execute();
+			connection.commit();
+			connection.close();
+			long end = System.currentTimeMillis();
+			System.out.printf("Import done in %d ms\n", (end - start));
+
+			this.countRows = 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
