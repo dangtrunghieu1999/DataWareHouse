3c237da7ce1ffd7da23283583638c49b0167f5a1
nguyenthithuyhang11 <55865873+nguyenthithuyhang11@users.noreply.github.com>
2020-08-11 00:15:14 +0700

    update FileDownLoad

--- DataWareHouse/src/model/Log.java
+++ DataWareHouse/src/model/Log.java
@@ -1,66 +1,76 @@
 package model;
 
-import java.sql.Date;
-
 public class Log {
 	private int id;
 	private String nameLog;
 	private String comment;
 	private String status;
 	private String urlLocal;
-	
+
 	private String time_download;
 	private String time_uploadStaging;
 	private String time_Warehouse;
-	
+
 	public String getComment() {
 		return comment;
 	}
+
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+
 	public String getNameLog() {
 		return nameLog;
 	}
+
 	public void setNameLog(String nameLog) {
 		this.nameLog = nameLog;
 	}
 
-	
-	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUrlLocal() {
 		return urlLocal;
 	}
+
 	public void setUrlLocal(String urlLocal) {
 		this.urlLocal = urlLocal;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public String getTime_download() {
 		return time_download;
 	}
+
 	public void setTime_download(String time_download) {
 		this.time_download = time_download;
 	}
+
 	public String getTime_uploadStaging() {
 		return time_uploadStaging;
 	}
+
 	public void setTime_uploadStraging(String time_uploadStaging) {
 		this.time_uploadStaging = time_uploadStaging;
 	}
+
 	public String getTime_Warehouse() {
 		return time_Warehouse;
 	}
+
 	public void setTime_Warehouse(String time_Warehouse) {
 		this.time_Warehouse = time_Warehouse;
 	}
