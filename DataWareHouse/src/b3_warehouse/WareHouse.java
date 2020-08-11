94c3dec203a76746b8970201ff6d9d1e82cb78e1
hieunguyen2803 <55368798+hieunguyen2803@users.noreply.github.com>
2020-08-11 14:49:35 +0700

    commit

--- DataWareHouse/src/b3_warehouse/WareHouse.java
+++ DataWareHouse/src/b3_warehouse/WareHouse.java
@@ -23,22 +23,14 @@ public class WareHouse {
 			connectDB = DBConnection.getConnection("Control");// kết nối đến
 																// db control
 			Statement st = connectDB.createStatement();// tạo statement
-			String query = "select table_name from config where id = " + id_config + " and flag = 'wh' "; // lấy
-																											// ra
-																											// tablename
-																											// có
-																											// gắn
-																											// flag
-																											// =
-																											// 'wh'
+			//lấy ra table name có gắn cờ flag= 'wh'
+			String query = "select table_name from config where id = " + id_config + " and flag = 'wh' "; // 
 			System.out.println(query);
-			ResultSet rs = st.executeQuery(query);// duyệt rs
-			if (rs.next()) {
-				String table_name = rs.getString("table_name");// nếu đúng
-																// thì trả về
-																// proceduce
-																// tương ứng
-
+			ResultSet rs = st.executeQuery(query);
+			// duyệt rs
+			if (rs.next()) { 
+				//nếu đúng thì trả về procedure tương ứng
+				String table_name = rs.getString("table_name");
 				switch (table_name) {
 				case STUDENT:
 					addStudentDB(table_name);
@@ -51,24 +43,24 @@ public class WareHouse {
 					break;
 
 				case REGISTER:
-				addRegister();
+					System.out.println("c");
 					break;
 
 				default:
 					break;
 				}
 			} else {
-				System.out.println("result set is Empty");// ngược lại thì...
+				//ngược lại thì thì send mail báo lỗi
+				System.out.println("result set is Empty");
+				SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Result set is Empty!");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 	}
-
-	public void addStudentDB(String tableName) {// phương thức addStudent sẽ
-												// tham chiếu tới Stored
-												// Procedure
+// phương thức addStudent nhận vào tableName tham chiếu tới procedure tương tứng trong Stored Procedure
+	public void addStudentDB(String tableName) {
 		long start = System.currentTimeMillis(); // time start trong hệ thống
 		Connection connectDB;
 		try {
@@ -81,10 +73,14 @@ public class WareHouse {
 			statement.execute();
 			long end = System.currentTimeMillis();// time end trong hệ thống
 			System.out.printf("Import done load in %d ms\n", (end - start));
-			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Successfully!");
-			updateProcessWh();// create
-			updateStatusLog();// create
+			//cập nhật flag thành 'st'
+			updateProcessWh();
+			//thay đổi status trong log thành 'Complete'
+			updateStatusLog();
+			//xóa dữ liệu ở table staging
+			truncateTableStaging(tableName);
 			System.out.println("Successfull");
+			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Successfull");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Fail!");
@@ -95,16 +91,10 @@ public class WareHouse {
 	public void updateProcessWh() {
 		Connection connectDB;
 		try {
-			connectDB = DBConnection.getConnection("Control");// kết nối đến
-																// db control
-			String query = "update config set config.flag = 'st' ";// khi config
-																	// đã được
-																	// load xong
-																	// thì cờ
-																	// từ 'wh'
-																	// đổi
-																	// thành
-																	// 'st'
+			connectDB = DBConnection.getConnection("Control");// kết nối đến db control
+			// khi load xong dữ liệu thì flag sẽ thay đổi 'wh' đổi thành 'st'
+			String query = "update config set config.flag = 'st' ";
+																	
 			PreparedStatement statement = connectDB.prepareStatement(query);
 			statement.execute();
 		} catch (SQLException e) {
@@ -116,26 +106,31 @@ public class WareHouse {
 	public void updateStatusLog() {
 		Connection connectDB;
 		try {
-			connectDB = DBConnection.getConnection("Control");// kết nối đến
-																// db control
-			String query = "update logs set logs.status = 'Complete' where logs.status = 'TR' ";// thay
-																								// đổi
-																								// trang
-																								// thái
-																								// khi
-																								// load
-																								// xong
-																								// từ'TR'
-																								// thành'Complete'
+			connectDB = DBConnection.getConnection("Control");// kết nối đến db control
+			//thay đổi trang thái khi load dữ liệu xong từ'TR' thành'Complete'												
+			String query = "update logs set logs.status = 'Complete' where logs.status = 'TR' ";
 			PreparedStatement statement = connectDB.prepareStatement(query);
 			statement.execute();
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-
+	// xóa dữ liệu cũ ở table staging
+	private void truncateTableStaging(String table) {
+		Connection connectDB;
+		try {
+			connectDB = DBConnection.getConnection("Staging");
+			String query = "TRUNCATE " + table;
+			PreparedStatement statement = connectDB.prepareStatement(query);
+			statement.execute();
+			statement.close();
+			connectDB.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	// phương thức addSubjectnhận vào tableName tham chiếu tới procedure tương tứng trong Stored Procedure
 	public void addSubjectDB(String tableName) {
 		long start = System.currentTimeMillis();
 		Connection connectDB;
@@ -148,11 +143,16 @@ public class WareHouse {
 			System.out.printf("Import done load in %d ms\n", (end - start));
 			updateProcessWh();
 			updateStatusLog();
+			truncateTableStaging(tableName);
+			System.out.println("Successfull");
+			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Successfull");
 		} catch (SQLException e) {
 			e.printStackTrace();
+			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Fail!");
 		}
 	}
 
+	// phương thức addClassDB nhận vào tableName tham chiếu tới procedure tương tứng trong Stored Procedure
 	public void addClassDB() {
 		long start = System.currentTimeMillis();
 		Connection connectDB;
@@ -163,28 +163,34 @@ public class WareHouse {
 			statement.execute();
 			long end = System.currentTimeMillis();
 			System.out.printf("Import done load in %d ms\n", (end - start));
+			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Successfull");
 		} catch (SQLException e) {
 			e.printStackTrace();
+			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Fail!");
 		}
 	}
-public void addRegister() {
+	// phương thức addRegister nhận vào tableName tham chiếu tới procedure tương tứng trong Stored Procedure
+	public void addRegister() {
 		long start = System.currentTimeMillis();
-			Connection connectDB;
-			try {
-				connectDB = DBConnection.getConnection("WareHouse");
-				String query = "{CALL addRegister()}";
-				PreparedStatement statement = connectDB.prepareStatement(query);
-				statement.execute();
-				long end = System.currentTimeMillis();
-				System.out.printf("Import done load in %d ms\n", (end - start));
-				updateProcessWh();
-				updateStatusLog();
-				statement.close();
-				connectDB.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		Connection connectDB;
+		try {
+			connectDB = DBConnection.getConnection("WareHouse");
+			String query = "{CALL addRegister()}";
+			PreparedStatement statement = connectDB.prepareStatement(query);
+			statement.execute();
+			long end = System.currentTimeMillis();
+			System.out.printf("Import done load in %d ms\n", (end - start));
+			updateProcessWh();
+			updateStatusLog();
+			statement.close();
+			connectDB.close();
+			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Successfull");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE, "Fail!");
+		}
 	}
+
 	public static void main(String[] args) {
 		WareHouse wh = new WareHouse();
 		wh.transformToWareHouse(1);
